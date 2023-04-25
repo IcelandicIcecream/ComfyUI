5ab8019cb278d0adfc25d4c5c41d62027dbe47c4
IcelandicIcecream <benjaminsinidol@gmail.com>
2023-04-25 08:59:02 +0000

    bruh

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/inconshreveable/log15 v3.0.0-testing.3+incompatible // indirect
 	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
+	github.com/inconshreveable/ngrok v0.0.0-20160531001041-a8e7fa486348 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- main.go
+++ main.go
@@ -3,32 +3,23 @@ package main
 import (
     "fmt"
     "os/exec"
+	"net/http"
 )
 
 func main() {
-    // cmd := exec.Command("bash", "bashScripts/install_dependencies.sh")
-    // output, err := cmd.Output()
-    // if err != nil {
-    //     fmt.Println("Error:", err)
-    // }
-    // fmt.Println(string(output))
-    go func() {
+
+    cmd := exec.Command("bash", "bashScripts/install_dependencies.sh")
+    output, err := cmd.Output()
+    if err != nil { 
+        fmt.Println("Error:", err)
+    }
+    fmt.Println(string(output))
+    
+    // Run ComfyUI
     cmd1 := exec.Command("bash", "bashScripts/run_comfy.sh")
     output1, err1 := cmd1.Output()
     if err1 != nil {
         fmt.Println("Error:", err1)
     }
     fmt.Println(string(output1))
-    }()
-    
-    go func() {
-    cmd2 := exec.Command("bash", "bashScripts/run_ngrok.sh")
-    output2, err2 := cmd2.Output()
-    if err2 != nil {
-        fmt.Println("Error:", err2)
-    }
-    fmt.Println(string(output2))
-    }()
-    
-    
 }
